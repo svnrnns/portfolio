693e288456f0de2f0e855f270ddd41e03dc15bf2
svnrnns <haruueel@gmail.com>
2024-05-13 21:40:18 +0200

    ref: scalability for app pages

---
+++ src/components/iconics/PageWrapper.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="screen-setup overflow-hidden flex flex-col items-center">
+    <div class="flex flex-col gap-3 pt-32 w-full page">
+      <span class="text-lg font-medium text-heading">{{ title }}</span>
+      <span class="text-detail">{{ text }}</span>
+      <slot />
+      <router-link
+        to="/"
+        class="flex items-center justify-start gap-1.5 mt-6 transition hover:underline cursor-pointer"
+      >
+        <ArrowUpRightIcon class="h-3 fill-heading" />
+        <span class="text-heading text-sm">Back</span>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import ArrowUpRightIcon from "/src/components/iconics/ArrowUpLeftIcon.vue";
+
+defineProps(["title", "text"]);
+</script>
--- src/views/Experience.vue
+++ src/views/Experience.vue
@@ -1,12 +1,9 @@
 <template>
   <div class="screen-setup overflow-hidden flex flex-col items-center">
-    <div class="flex flex-col gap-3 pt-32 w-full page">
-      <span class="text-lg font-medium text-heading"
-        >Enterprise Experience</span
-      >
-      <span class="text-detail"
-        >List of most relevant projects I have been part of.</span
-      >
+    <PageWrapper
+      :title="'Enterprise Experience'"
+      :text="'List of most relevant projects I have been part of.'"
+    >
       <div class="w-full flex flex-col divide-y divide-font/30 mt-6 px-1.5">
         <div
           class="flex items-center justify-between py-3 cursor-pointer"
@@ -19,18 +16,19 @@
           <span class="text-sm">{{ project.year }}</span>
         </div>
       </div>
-      <router-link
+    </PageWrapper>
+    <!-- <router-link
         to="/"
         class="flex items-center justify-start gap-1.5 mt-6 transition hover:underline cursor-pointer"
       >
         <ArrowUpRightIcon class="h-3 fill-heading" />
         <span class="text-heading text-sm">Back</span>
-      </router-link>
-    </div>
+      </router-link> -->
   </div>
 </template>
 
 <script setup>
+import PageWrapper from "../components/iconics/PageWrapper.vue";
 import ArrowUpRightIcon from "/src/components/iconics/ArrowUpLeftIcon.vue";
 import projects from "/src/api/projects.js";
 </script>
