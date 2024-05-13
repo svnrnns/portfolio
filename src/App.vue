b94a97da7585903bc86be8c2033675b9df25c465
svnrnns <haruueel@gmail.com>
2024-05-13 21:16:43 +0200

    feat: better visual language for anchors

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,3 @@
 <template>
-  <!-- All Icon - Font Awesome Pro 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2024 Fonticons, Inc.-->
   <router-view />
 </template>
--- src/views/components/ActiveJob.vue
+++ src/views/components/ActiveJob.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="flex flex-col gap-3">
-    <div class="flex gap-3 items-center slide-in-delay" style="--delay: 6">
-      <ShieldCheckIcon class="h-4 fill-font" />
-      <span>Currently working at</span>
-    </div>
-    <span
-      class="text-lg font-medium text-heading slide-in-delay"
-      style="--delay: 7"
-      >Zeus - Smart Visual Data</span
+  <div class="flex flex-col gap-6">
+    <span class="text-sm text-detail slide-in-delay" style="--delay: 6"
+      >Currently working at</span
     >
+    <a
+      href="https://zeus.vision"
+      target="_blank"
+      class="text-lg font-medium text-heading slide-in-delay -mt-3 hover:underline"
+      style="--delay: 7"
+      >Zeus - Smart Visual Data</a
+    >
+
     <span class="slide-in-delay" style="--delay: 7">Frontend Developer</span>
     <div
       class="w-full h-px bg-detail/30 slide-in-delay my-9"
@@ -16,7 +18,3 @@
     ></div>
   </div>
 </template>
-
-<script setup>
-import ShieldCheckIcon from "/src/components/iconics/ShieldCheckIcon.vue";
-</script>
