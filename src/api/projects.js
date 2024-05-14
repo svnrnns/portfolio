aed32eaaa944e64468fd91c61c479b1532a919e1
svnrnns <haruueel@gmail.com>
2024-05-14 18:15:24 +0200

    docs: added icex in experience

--- src/api/projects.js
+++ src/api/projects.js
@@ -1,13 +1,19 @@
 export default [
   {
-    name: "Baleria Control Tower",
-    article: "balearia-article",
-    company: "Zeus - Smart Visual Data",
-    year: 2023,
+    name: "ICEX Map (Project Sprint)",
+    article: "icex-map-article",
+    company: "Zeus- Smart Visual Data",
+    year: 2024,
   },
   {
     name: "Ucersa Dashboard",
     article: "ucersa-article",
+    company: "Zeus - Smart Visual Data",
+    year: 2024,
+  },
+  {
+    name: "Baleria Control Tower",
+    article: "balearia-article",
     company: "Zeus - Smart Visual Data",
     year: 2023,
   },
