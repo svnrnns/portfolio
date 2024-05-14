089a3308196c7c47cc98e6b1007ae17e999469ad
svnrnns <haruueel@gmail.com>
2024-05-14 18:37:53 +0200

    fix: removed vue-popper import in main.js

--- src/main.js
+++ src/main.js
@@ -4,10 +4,6 @@ import App from "./App.vue";
 
 const app = createApp(App);
 
-// Vue Popper
-import Popper from "vue3-popper";
-app.component("Popper", Popper);
-
 // Vue Router
 import router from "./router/routes";
 app.use(router);
