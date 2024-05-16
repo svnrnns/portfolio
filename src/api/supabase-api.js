d58b8dc96e7d981b038d1a45d6c01dbd71d396ac
Miguel Montesinos <m.jimenez@zeus.local>
2024-05-16 09:00:05 +0200

    chore: removed serverless functions (ty vercel)

--- src/api/supabase-api.js
+++ src/api/supabase-api.js
@@ -1,17 +1,6 @@
 import { ref, computed } from "vue";
-import { createClient } from "@supabase/supabase-js";
+import { supabase } from "./supabase-client";
 
-const isProd = import.meta.env.VITE_APP_IS_PROD === "true";
-let supabase = null;
-
-function createIfNotExistsSupabaseClient() {
-  if (supabase == null) {
-    const supabaseUrl = import.meta.env.VITE_APP_SUPABASE_URL;
-    const supabaseKey = import.meta.env.VITE_APP_SUPABASE_KEY;
-
-    supabase = createClient(supabaseUrl, supabaseKey);
-  }
-}
 function processResponse(supabaseResponse) {
   let response = null;
   let error = null;
@@ -37,32 +26,21 @@ export function getArticle(code) {
   const error = ref(null);
   const loading = ref(true);
 
-  if (isProd == true) {
-    fetch("/api/article?code=" + code).then((serverResponse) => {
-      serverResponse.json().then((processedData) => {
-        response.value = processedData.response[0];
-        error.value = processedData.error;
-      });
+  supabase
+    .from("articles")
+    .select("*")
+    .eq("code", code)
+    .then((supabaseResponse) => {
+      const {
+        response: responseData,
+        error: errorData,
+        loading: loadingData,
+      } = processResponse(supabaseResponse);
+
+      response.value = responseData[0];
+      error.value = errorData;
+      loading.value = loadingData;
     });
-  } else {
-    createIfNotExistsSupabaseClient();
-
-    supabase
-      .from("articles")
-      .select("*")
-      .eq("code", code)
-      .then((supabaseResponse) => {
-        const {
-          response: responseData,
-          error: errorData,
-          loading: loadingData,
-        } = processResponse(supabaseResponse);
-
-        response.value = responseData[0];
-        error.value = errorData;
-        loading.value = loadingData;
-      });
-  }
 
   return computed(() => {
     return {
