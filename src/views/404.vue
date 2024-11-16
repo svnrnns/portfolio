f983e16ff6d1d9d2803b0015cb35319ed099b12e
svnrnns <haruueel@gmail.com>
2024-11-16 11:01:15 +0100

    fix: removed crashing import in 404.vue

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,21 +1,13 @@
 <template>
-  <PageWrapper
-    title="404 - Page not found"
-    detail="man"
-    to="/"
-  >
-    <div class="h-32 w-32 flex items-start mt-6 rounded-xl overflow-hidden">
-      <img
-        src="https://cdn.betterttv.net/emote/6060eb0d7493072efdeb63a2/3x.webp"
-        class="rounded-xl h-full w-full"
-        draggable="false"
-        loading="lazy"
-        alt="sadgers"
-      />
-    </div>
-  </PageWrapper>
+  <div class="h-32 w-32 flex items-start mt-6 rounded-xl overflow-hidden">
+    <img
+      src="https://cdn.betterttv.net/emote/6060eb0d7493072efdeb63a2/3x.webp"
+      class="rounded-xl h-full w-full"
+      draggable="false"
+      loading="lazy"
+      alt="sadgers"
+    />
+  </div>
 </template>
 
-<script setup>
-import PageWrapper from '../components/PageWrapper.vue';
-</script>
+<script setup></script>
