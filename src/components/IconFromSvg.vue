98fbbb51bf0b2e8d68fd7384412d4f9fead8497a
svnrnns <haruueel@gmail.com>
2024-11-16 13:22:55 +0100

    feat: home side navbar

---
+++ src/components/IconFromSvg.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="size-8 overflow-hidden p-1.5 rounded-lg shadow"
+    :style="{ background: bg }"
+  >
+    <component
+      :is="icon"
+      class="size-full"
+    />
+  </div>
+</template>
+
+<script setup>
+defineProps(['icon', 'bg']);
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="flex flex-col items-center gap-44 mt-36">
+  <div class="flex flex-col items-center mt-12 md:mt-36">
+    <HomeSideNavbar />
     <HomeIntro class="slide-in-delay" />
     <HomeIndex class="slide-in-delay" />
     <HomeWork
@@ -8,8 +9,9 @@
       position="Senior Frontend Developer"
       from="May 2023"
       icon-bg="#f93f55"
-      :icon="ZeusIcon"
       item-name="zeus"
+      html-id="zeus-work"
+      :icon="ZeusIcon"
     />
     <HomeCredits />
   </div>
@@ -20,6 +22,7 @@ import HomeIntro from './home/HomeIntro.vue';
 import HomeIndex from './home/HomeIndex.vue';
 import HomeWork from './home/HomeWork.vue';
 import HomeCredits from './home/HomeCredits.vue';
+import HomeSideNavbar from './home/HomeSideNavbar.vue';
 
 import ZeusIcon from '@/components/iconics/ZeusIcon.vue';
 </script>
