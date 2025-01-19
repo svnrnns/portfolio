48c38bc18b3f2b5e1a00c8bd69ffdb4755f3dcda
svnrnns <haruueel@gmail.com>
2025-01-19 16:46:08 +0100

    feat: added bottom fade on scroll for position title

--- src/views/home/HomePosition.vue
+++ src/views/home/HomePosition.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 import type { Position } from '@/types/Position';
 import type { IconConstructor } from '@/types/IconConstructor';
-import type { PropType } from 'vue';
+import { onBeforeUnmount, onMounted, type PropType } from 'vue';
 import IconWithBackground from '@/components/IconWithBackground.vue';
 import { dateToMonthYearString } from '@/fns/dates';
 import MakrdownToHTML from '@/components/MakrdownToHTML.vue';
+import { ref } from 'vue';
 
-defineProps({
+const props = defineProps({
   data: {
     type: Object as PropType<Position>,
     required: true,
@@ -20,22 +21,51 @@ defineProps({
     required: true,
   },
 });
+
+const isTitleSticky = ref<boolean>(false);
+
+const checkIfTitleIsSticky = (titleElement: HTMLElement | null) => {
+  if (!titleElement) return;
+
+  const rect = titleElement.getBoundingClientRect();
+  if (rect.y <= 0) isTitleSticky.value = true;
+  else isTitleSticky.value = false;
+};
+
+onMounted(() => {
+  const titleElement = document.getElementById(props.htmlId);
+
+  window.addEventListener('scroll', () => checkIfTitleIsSticky(titleElement));
+
+  onBeforeUnmount(() => {
+    window.removeEventListener('scroll', () =>
+      checkIfTitleIsSticky(titleElement)
+    );
+  });
+});
 </script>
 
 <template>
   <div class="w-full flex flex-col gap-1.5">
-    <div
-      class="w-full px-20.5 py-2 flex items-center gap-3 bg-body sticky top-0"
-      :id="htmlId"
-    >
-      <IconWithBackground
-        :bg="iconConstructor.bg"
-        :icon="iconConstructor.icon"
-      />
-      <span class="gigantic-text text-heading font-semibold">
-        {{ data.title }}
-      </span>
+    <div class="w-full sticky top-0">
+      <div
+        class="w-full px-20.5 py-4 flex items-center gap-3 bg-body"
+        :id="htmlId"
+      >
+        <IconWithBackground
+          :bg="iconConstructor.bg"
+          :icon="iconConstructor.icon"
+        />
+        <span class="gigantic-text text-heading font-semibold">
+          {{ data.title }}
+        </span>
+      </div>
+      <div
+        class="w-full absolute top-full h-60 bg-gradient-to-b from-body to-body/0 opacity-0 transition-all duration-500"
+        :class="{ '!opacity-100': isTitleSticky }"
+      ></div>
     </div>
+
     <span class="font-mono tiny-text text-detail w-full page-block-raw mx-auto">
       {{ dateToMonthYearString(data.from) }} -
       <span :class="{ 'text-success font-medium': !data.to }">
